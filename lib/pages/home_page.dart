7453c110aedd5e11a6a3223d2caaadd764d808c4
SkyMalcolm <sky.lennartsson@iths.se>
2022-11-10 10:15:58 +0100

    buildup of firebase auth

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:recipe_app/auth.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
