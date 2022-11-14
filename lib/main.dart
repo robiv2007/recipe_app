412812c8c0704ad1e68a1f61de1ea12f857f553a
SkyMalcolm <sky.lennartsson@iths.se>
2022-11-14 15:11:58 +0100

    hehe

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: LoginPage(), //const MyHomePage(title: 'Recipe Spot')
+      home: const LoginPage(),
     );
   }
 }
