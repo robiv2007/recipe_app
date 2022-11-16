ea6b7ef84db5ae083e3e52faccf1e681496e9d91
Robert <90681378+robiv2007@users.noreply.github.com>
2022-11-16 13:07:15 +0100

    Read data from firebase

--- lib/pages/recipe_overview_screen.dart
+++ lib/pages/recipe_overview_screen.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_app/cardView.dart';
 import 'package:recipe_app/main.dart';
 import '../auth.dart';
 import 'addRecipe.dart';
+
 class RecipeOverviewScreen extends StatefulWidget {
   const RecipeOverviewScreen({super.key});
 
@@ -12,63 +15,116 @@ class RecipeOverviewScreen extends StatefulWidget {
 
 class _RecipeOverviewScreenState extends State<RecipeOverviewScreen> {
   final Auth _auth = Auth();
+
+  final User? user = Auth().currentUser;
+
+  List<String> docIds = [];
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance.collection('Recipes').get().then(
+          (snapshot) => snapshot.docs.forEach((element) {
+            print(element.reference);
+            docIds.add(element.reference.id);
+          }),
+        );
+  }
+
+  final CollectionReference _recipes =
+      FirebaseFirestore.instance.collection('Recipes');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(color: Colors.orange),
-                child: Text(
-                  "MENU",
-                  style: TextStyle(fontSize: 20),
-                ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.orange),
+              child: Text(
+                "MENU",
+                style: TextStyle(fontSize: 20),
               ),
-             ListTile(/*Add Recipe button*/
+            ),
+            ListTile(
+              /*Add Recipe button*/
               leading: Icon(Icons.add_circle_outline),
               title: const Text("Add Recipe"),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> addRecipes(id: "", title: "name", description: "description", cookTime: "cookTime",thumbnailUrl: "thumbnailUrl")));
-            },
-          ),
-              ListTile(
-                leading: Icon(Icons.favorite_border),
-                title: Text("Favorite"),
-                onTap: () {
-                  Navigator.of(context).push(
+              onTap: () {
+                Navigator.push(
+                    context,
                     MaterialPageRoute(
-                      builder: (context) => MyHomePage(title: "recipe"),
-                    ),
+                        builder: (context) => addRecipes(
+                            id: "",
+                            title: "name",
+                            description: "description",
+                            cookTime: "cookTime",
+                            thumbnailUrl: "thumbnailUrl")));
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite_border),
+              title: Text("Favorite"),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(title: "recipe"),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('LOG OUT'),
+              onTap: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Recipe"),
+      ),
+      body: StreamBuilder(
+          stream: _recipes.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                padding: EdgeInsets.all(16),
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return RecipeCard(
+                    title: documentSnapshot['name'],
+                    cookTime: documentSnapshot['CookTime'],
+                    thumbnailUrl: documentSnapshot['thumbnailUrl'],
                   );
                 },
-              ),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text('LOG OUT'),
-                onTap: () async {
-                  await _auth.signOut();
-                },
-              )
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          title: Text("Recipe"),
-        ),
-        body: ListView.builder(
-          padding: EdgeInsets.all(16),
-          itemBuilder: (context, index) {
-            return RecipeCard(
-              title: "pizza",
-              cookTime: "40",
-              rating: "8",
-              thumbnailUrl:
-                  "https://assets.biggreenegg.eu/app/uploads/2021/04/30120446/topimage-pizza-margherita-2021m05-800x533-1.jpg",
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ));
+          }),
+      // body: FutureBuilder(
+      //   future: getDocId(),
+      //   builder: (context, snapshot) {
+      //     return ListView.builder(
+      //       itemCount: docIds.length,
+      //       padding: EdgeInsets.all(16),
+      //       itemBuilder: (context, index) {
+      //         return RecipeCard(
+      //           title: "",
+      //           cookTime: "40",
+      //           thumbnailUrl:
+      //               "https://assets.biggreenegg.eu/app/uploads/2021/04/30120446/topimage-pizza-margherita-2021m05-800x533-1.jpg",
+      //         );
+      //       },
+      //     );
+      //   },
+      // ),
+    );
   }
 }
-
